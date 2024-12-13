d7b664501381da9cf7b3a49e42e6d8e166a785c7
AtcsuHollow <xianshengfuchu@gmail.com>
2024-12-13 13:49:54 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,8 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 72),
-     
-        }, 100, "easeInOutExpo");
+          scrollTop: (target.offset().top - 72)
+        }, 0, "easeInOutExpo");
         return false;
       }
     }
